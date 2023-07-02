1cb6d466ba63859d28302ea9440e1b8ffeb42ccd
kalebe misael <kalebemisael@gmail.com>
2023-07-02 16:53:59 -0300

    :construction model and rulers button color:

--- iQuiz/QuestionViewController.swift
+++ iQuiz/QuestionViewController.swift
@@ -13,8 +13,18 @@ class QuestionViewController: UIViewController {
         
         if userGotAnswerRight {
             punctuation += 1
+            
+            sender.backgroundColor = UIColor(red: 11/255, green: 161/255, blue: 53/255, alpha: 1.0)
             print("acertou")
+        }else{
+            sender.backgroundColor = UIColor(red: 211/255, green: 17/255, blue: 17/255, alpha: 1.0)
         }
+        
+        if questionNumber < questions.count - 1 {
+            questionNumber += 1
+            Timer.scheduledTimer(timeInterval: 0.5, target: self, selector: #selector(configQuestion), userInfo: nil, repeats: false)
+        }
+        
     }
     
     override func viewDidLoad() {
@@ -33,11 +43,12 @@ class QuestionViewController: UIViewController {
         }
     }
     
-    func configQuestion(){
+    @objc func configQuestion(){
         titleQuestionLabel.text = questions[questionNumber].title
         for button in buttonsResponse {
             let titleButton = questions[questionNumber].responses[button.tag]
             button.setTitle(titleButton, for: .normal)
+            button.backgroundColor = UIColor(red: 116/255, green: 50/255, blue: 255/255, alpha: 1.0)
         }
     }
 }
