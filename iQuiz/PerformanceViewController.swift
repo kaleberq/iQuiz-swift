f76d74abe3ed7bd60019ca5effafe91bf6c1c956
kalebe misael <kalebemisael@gmail.com>
2023-07-02 17:19:48 -0300

    :construction: page percent quiz

---
+++ iQuiz/PerformanceViewController.swift
@@ -0,0 +1,18 @@
+import UIKit
+
+class PerformanceViewController: UIViewController {
+
+    @IBOutlet weak var resultLabel: UILabel!
+    @IBOutlet weak var percentLabel: UILabel!
+    @IBOutlet weak var buttonRestarQuiz: UIButton!
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        configLayout()
+    }
+    
+    func configLayout(){
+        buttonRestarQuiz.layer.cornerRadius = 12.0
+    }
+    
+}
